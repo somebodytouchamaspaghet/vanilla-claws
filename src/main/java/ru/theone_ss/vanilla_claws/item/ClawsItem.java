6fba2f8731457a5f769aed439998647e917a0c55
somebodytouchamaspaghet <146813760+somebodytouchamaspaghet@users.noreply.github.com>
2023-10-07 17:39:51 +0800

    Update ClawsItem.java

--- src/main/java/ru/theone_ss/vanilla_claws/item/ClawsItem.java
+++ src/main/java/ru/theone_ss/vanilla_claws/item/ClawsItem.java
@@ -23,7 +23,7 @@ public class ClawsItem extends SwordItem {
         ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
         builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Weapon modifier", this.attackDamage, EntityAttributeModifier.Operation.ADDITION));
         builder.put(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Weapon modifier", attackSpeed, EntityAttributeModifier.Operation.ADDITION));
-        builder.put(ReachEntityAttributes.ATTACK_RANGE, new EntityAttributeModifier("Weapon modifier", -2.0, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(ReachEntityAttributes.ATTACK_RANGE, new EntityAttributeModifier("Weapon modifier", -1.5, EntityAttributeModifier.Operation.ADDITION));
         this.attributeModifiers = builder.build();
     }
 
